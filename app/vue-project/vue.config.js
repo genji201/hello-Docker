67c6366f4f6289024e9dc486e68f1a0fe978528b
KannoStanfoot <kanno@stanfoot.com>
2019-11-13 17:06:12 +0900

    watchオプションをdevServerに追加した。

--- app/vue-project/vue.config.js
+++ app/vue-project/vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   devServer: {
     port: 8080,
     host: `0.0.0.0`,
-    disableHostCheck: true
+    disableHostCheck: true,
+    watchOptions: {
+      poll: true
+    }
   }
 };
